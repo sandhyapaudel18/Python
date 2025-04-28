bbd545751bf23209ecb4b2e0d70f72c87d4d45f8
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2025-04-28 18:58:39 +0545

    added

---
+++ Basics/dictionary.py
@@ -0,0 +1,36 @@
+#stores data in key:value pair form
+#unorderd,mutable,but not duplicate value like list does
+
+info={
+    "name":"sandhya",
+    "gpa":10,
+    "age":21 ,  
+    "is_logged":True,
+    "ll" :[1,2,3,4,5],
+    "tt":("sandhya","paudel"),
+    12:"data"
+}
+info["name"]="paudel"
+null_dict={}
+# print(null_dict)
+
+
+# print(student["subjects"]["dsa"]) to access dict-dict-item
+#methods .keys()=only keys
+#.values()=only values
+#.items()=return all key and value as tuples
+#.get("key_name")=value as per keys
+#.update(newItem)=inserts new item to the dictionary
+
+student={
+    "name":"akash",
+    "subjects":{
+        "dsa":99,
+        "os":67
+    }
+}
+# print(student.values())
+print(list(student.keys()))
+print(len(student))
+# print(student.items())
+# print(student.get("name"))
