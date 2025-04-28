cdeca20a0e25911d623ca6680640e60d91bec995
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2025-04-28 13:05:56 +0545

    added

--- Basics/quiz.py
+++ Basics/quiz.py
@@ -1,6 +1,27 @@
 #print even numbers between 1 to 10 and at last print [We have 4 even numbers]
-for number in range(1,10,1):
-    if number%2==0:
-        print(number)
+# for number in range(1,10,1):
+#     if number%2==0:
+#         print(number)
 
-print("We have 4 even numbers ")       
+# print("We have 4 even numbers ")       
+
+#wap to ask user to enter their 3 favourites movie and store in movies
+
+movies =[]
+# for i in range(3):
+#     movies.append(input("enter name"))
+# print(movies)
+
+
+#wap to check if list contains palindrome of elements
+# [1,2,3,2,1] [1,"abc","abc",1]
+
+name=[1,2,3,2,1]
+m=[]
+m=name.copy()
+m.reverse()
+if m==name:
+    print("palindrome")
+else:
+    print("not palindrome")
+
--- Basics/tuples.py
+++ Basics/tuples.py
@@ -3,4 +3,19 @@ tuple=(1,) #why comma because py thinks it as normal interger insitead of tuples
 # print(tuple)
 
 tconcept=(4,5,34,3)
-print(tconcept.index(5))
+# print(tconcept.index(5))
+
+# print(tconcept.count(5))
+#tuples questions here
+#wap to count the number of students with A
+
+tt=("C","D","T","Y","C","C")
+# print(tt.count("C"))
+
+
+ll=[]
+for i in range(len(tt)):
+    ll.append(tt[i])
+ll.sort()    
+print(ll) 
+  
