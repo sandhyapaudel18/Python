d0c25288204efdffadffaca6b6071e30986f5856
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2025-05-01 19:32:32 +0545

    added

---
+++ NumPhy/Project/dataset.py
@@ -0,0 +1,15 @@
+"""
+| Order ID | Customer Age | Product Category | Product Price | Quantity | Discount (%) | Delivery Time (days) | Rating (out of 5) |
+|----------|---------------|------------------|----------------|----------|----------------|------------------------|--------------------|
+| 1001     | 28            | Electronics       | 249.99         | 1        | 5              | 3                      | 4.2                |
+| 1002     | 35            | Clothing          | 39.99          | 2        | 0              | 5                      | 4.0                |
+| 1003     | 22            | Books             | 12.49          | 3        | 10             | 2                      | 4.7                |
+| 1004     | 31            | Electronics       | 499.99         | 1        | 15             | 4                      | 3.9                |
+| 1005     | 45            | Home              | 89.99          | 1        | 0              | 6                      | 4.3                |
+| ...      | ...           | ...               | ...            | ...      | ...            | ...                    | ...                |
+
+
+"""
+
+import numpy as np
+revenue=np.array([])
