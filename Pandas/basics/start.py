86c6f7a8da821fed1de53cfaf933c496d36b4abb
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2025-05-02 18:22:52 +0545

    added

--- Pandas/basics/start.py
+++ Pandas/basics/start.py
@@ -1,2 +1,8 @@
 import pandas as pd
-print(pd.__version__)
+#series is 1d array in pandas
+#dataframe is normally 2d array having rows and columns in pandas
+
+#encoding=files to computer understandble form "latin1"
+df= pd.read_csv("students.xlsx",encoding="latin1")
+print(df)
+
